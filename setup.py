36ce47a72c84d582cb68128453c41a1328cf01fe
sapphire <blueishsapphire0@gmail.com>
2023-10-31 18:44:03 -0400

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+	name='pysoaper',
+	version='0.1',
+	description='A clean testing framework for python.',
+	author='BlueishSapphire',
+	author_email='blueishsapphire1@gmail.com',
+	packages=['pysoaper'],
+	install_requires=[]
+)
