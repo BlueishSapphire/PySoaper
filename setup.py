982b48c130e4044060f507f57392869fefbbd6b3
sapphire <blueishsapphire0@gmail.com>
2023-10-31 18:48:32 -0400

    Changed name to soaper (from pysoaper)

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-	name='pysoaper',
+	name='soaper',
 	version='0.1',
 	description='A clean testing framework for python.',
 	author='BlueishSapphire',
 	author_email='blueishsapphire1@gmail.com',
-	packages=['pysoaper'],
+	packages=['soaper'],
 	install_requires=[]
 )
