5767dd731fdd26263e71b97f3ca4b526f1e7ff88
sapphire <blueishsapphire0@gmail.com>
2023-10-31 20:05:51 -0400

    Added long description

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup
 
+
+with open("README.md", "r") as f
+	long_description = f.read()
+
+
 setup(
 	name='soaper',
 	version='0.1',
 	description='A clean testing framework for python.',
+	long_description=long_description,
 	author='BlueishSapphire',
 	author_email='blueishsapphire1@gmail.com',
 	packages=['soaper'],
